a9870d231e043e27c893d429e46d1f2c004b8ae8
chenyk1219 <150651687+chenyk1219@users.noreply.github.com>
2024-01-09 00:28:44 +0800

    add ports

---
+++ ports/main.py
@@ -0,0 +1,118 @@
+# -*- coding: UTF-8 -*-
+"""
+@File    ：signal.py
+@Author  ：https://t.me/iNextOpsChannel
+@Date    ：2024/1/8 2:49
+"""
+
+from pyrogram import Client
+from pagermaid.listener import listener
+from pagermaid.utils import Message
+from pagermaid.utils import alias_command
+import subprocess
+import psutil
+import prettytable
+import time
+
+
+@listener(is_plugin=True, outgoing=True, command=alias_command("ports"), description="检查服务器端口连接情况")
+async def ports(bot: Client, context: Message):
+    context_list = []
+
+    port_list = context.parameter
+
+    startTime = time.time()
+
+    if port_list is None or len(port_list) == 0:
+        context_list.append("为指定端口号，比如 ,ports 22，默认检测22端口")
+        await context.edit("\n".join(context_list))
+        port = 22
+    else:
+        try:
+            port = int(port_list[0])
+            if port < 0 or port > 65535:
+                context_list.append("端口号范围为0-65535")
+                await context.edit("\n".join(context_list))
+                return
+        except:
+            context_list.append("端口号必须为数字")
+            await context.edit("\n".join(context_list))
+            return
+    # define data structure for each connection, each ip is unique unit
+    ipaddress = {
+        'ipaddress': None,
+        'counts': 0,
+        'stat': {
+            'established': 0,
+            'time_wait': 0,
+            'others': 0
+        }
+    }
+
+    # define data structure for statistics
+    statistics = {
+        'portIsUsed': False,
+        'portUsedCounts': 0,
+        'portPeerList': [
+            {
+                'ipaddress': None,
+                'counts': 0,
+                'stat': {
+                    'established': 0,
+                    'time_wait': 0,
+                    'others': 0
+                },
+            },
+        ]
+    }
+
+    tmp_portPeerList = list()
+    portPeerSet = set()
+    netstat = psutil.net_connections()
+
+    # get all ip address only for statistics data
+    for i, sconn in enumerate(netstat):
+
+        if port in sconn.laddr:
+            statistics['portIsUsed'] = True
+            if len(sconn.raddr) != 0:
+                statistics['portUsedCounts'] += 1
+                ipaddress['ipaddress'] = sconn.raddr[0]
+                tmp_portPeerList.append(str(ipaddress))  # dict() list() set() is unhashable type, collections.Counter
+
+    for ip in tmp_portPeerList:
+        portPeerSet.add(str(ip))  # remove duplicated ip address using set()
+
+    for member in portPeerSet:
+        statistics['portPeerList'].append(eval(member))
+
+    # add statistics data for each ip address
+    for sconn in netstat:
+        if port in sconn.laddr:
+            if len(sconn.raddr) != 0:
+                for i, item in enumerate(statistics['portPeerList']):
+                    if item['ipaddress'] == sconn.raddr[0]:
+                        statistics['portPeerList'][i]['counts'] += 1
+                        if sconn.status == 'ESTABLISHED':
+                            statistics['portPeerList'][i]['stat']['established'] += 1
+                        elif sconn.status == 'TIME_WAIT':
+                            statistics['portPeerList'][i]['stat']['time_wait'] += 1
+                        else:
+                            statistics['portPeerList'][i]['stat']['others'] += 1
+
+    # print statistics result using prettytable
+    if statistics['portIsUsed']:
+        context_list.append(" %s 端口总连接数为： %d." % (port, statistics['portUsedCounts']))
+        context_list.append(" ")
+        for i, ip in enumerate(statistics['portPeerList']):
+            if ip['ipaddress'] is not None:
+                context_list.append(
+                    f"{ip['ipaddress']}，总连接数为：{ip['counts']}，已建立连接数为：{ip['stat']['established']}，等待连接数为：{ip['stat']['time_wait']}，其他连接数为：{ip['stat']['others']}")
+        context_list.append(" ")
+    else:
+        context_list.append(' %s 端口没有连接，请确保端口正在监听或正在使用中' % port)
+
+    endTime = time.time()
+    context_list.append("耗时(s): %s seconds." % (endTime - startTime))
+
+    await context.edit("\n".join(context_list))
