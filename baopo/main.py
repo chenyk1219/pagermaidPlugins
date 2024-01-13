79ccf600f8d6f00092a76fa7eba9e4d6008e3e15
chenyk1219 <150651687+chenyk1219@users.noreply.github.com>
2024-01-13 16:14:54 +0800

    新增有登录权限的用户的查询

--- baopo/main.py
+++ baopo/main.py
@@ -25,29 +25,44 @@ async def baopo(bot: Client, context: Message):
     await context.edit("开始检查爆破IP...")
     templ = "%-15s %15s"
     context_list = []
+    shell = "grep sh /etc/passwd | awk -F':' '{print $1}'"
+    who_can_login = subprocess.Popen(shell, shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+    context_list.append("用登录权限的用户：（建议设置复杂的密码）")
+    context_list.append(who_can_login)
+    context_list.append('=====================')
     context_list.append(templ % ("爆破IP", "爆破次数"))
     if os.path.exists('/var/log/secure'):
         shell = "grep \"Invalid user\" /var/log/secure | awk -F'from' '{print $2}' | awk -F' ' '{print $1}' | sort | uniq -c | sort -nr | head -n10"
         faild_log = subprocess.Popen(shell, shell=True, stdout=subprocess.PIPE,
                                      stderr=subprocess.STDOUT).stdout.read().decode(
             'utf-8').split('\n')
-        for line in faild_log:
-            if line:
-                line = line.strip()
-                context_list.append(templ % (line.split(' ')[1], line.split(' ')[0]))
+        if faild_log:
 
-        await context.edit("\n".join(context_list))
+            for line in faild_log:
+                if line:
+                    line = line.strip()
+                    context_list.append(templ % (line.split(' ')[1], line.split(' ')[0]))
+
+            await context.edit("\n".join(context_list))
+        else:
+            context_list.append("没有记录")
+            await context.edit("\n".join(context_list))
     elif os.path.exists('/var/log/auth.log'):
         shell = "grep \"Failed\" /var/log/auth.log | awk -F'from' '{print $2}' | awk -F' ' '{print $1}' | sort | uniq -c | sort -nr | head -n10"
         faild_log = subprocess.Popen(shell, shell=True, stdout=subprocess.PIPE,
                                      stderr=subprocess.STDOUT).stdout.read().decode(
             'utf-8').split('\n')
-        for line in faild_log:
-            if line:
-                line = line.strip()
-                context_list.append(templ % (line.split(' ')[1], line.split(' ')[0]))
+        if faild_log:
 
-        await context.edit("\n".join(context_list))
+            for line in faild_log:
+                if line:
+                    line = line.strip()
+                    context_list.append(templ % (line.split(' ')[1], line.split(' ')[0]))
+
+            await context.edit("\n".join(context_list))
+        else:
+            context_list.append("没有记录")
+            await context.edit("\n".join(context_list))
     else:
-        await context.edit("error：无法找到日志文件，爆破IP查询失败！")
+        await context.edit("error：无法找到日志文件，请开启登陆审计记录，爆破IP查询失败！")
 
